cec27dbecbeb9e2bea256ee8a7d9b98898648273
KARTIKE KISHORE <kartikekishore@gmail.com>
2021-12-31 18:23:45 +0530

    Added mailchimps API

--- app.js
+++ app.js
@@ -1,15 +1,58 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+const https = require("https");
+const { options } = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/signup.html");
 });
+app.post("/",function(req,res){
+  console.log(req.body);
+
+  var data = {
+    members:[
+      {
+        email_address:req.body.email,
+        status: "subscribed",
+        NAME:req.body.name,
+      }
+    ]
+  }
+  const jsonData=JSON.stringify(data);
+  const url="https://us20.api.mailchimp.com/3.0/lists/5cab55efa9";
+  const options= {
+    method:"POST",
+    auth:"kary:30b3ff7bce30d47af926ba7984ecf300-us20"
+ }
+ const request= https.request(url,options,function(response){
+    response.on("data",function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
+});
 
 app.listen(3000, function () {
   console.log("listening on port 3000.");
 });
+
+
+// mailchimp.setConfig({
+//   apiKey: "30b3ff7bce30d47af926ba7984ecf300-us20",//5cab55efa9
+//   server: "us20",
+// });
+
+// async function run() {
+//   const response = await mailchimp.ping.get();
+//   console.log(response);
+// }
+
+// run();
